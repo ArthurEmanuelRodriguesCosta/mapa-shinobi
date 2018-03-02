a4edfd2562205d5043e8998b512e7001a5efc9af
ArthurEmanuelRodriguesCosta <arthur.costa@ccc.ufcg.edu.br>
2018-03-02 05:36:20 +0000

    adicionado mapa interativo do lab 3

---
+++ 5-so-voces/1-cria-mapa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# a expressão js que decide os fills baseados em uma escala
+# EXP_ESCALA='z = d3.scaleSequential(d3.interpolateViridis).domain([0, 100]),
+#             d.features.forEach(f => f.properties.fill = z(f.properties["Percentual Aprendizado Adequado (%)"])),
+#             d'
+EXP_ESCALA='z = d3.scaleThreshold().domain([0, 1, 3, 5]).range(d3.schemeBrBG[4]),
+            d.features.forEach(f => f.properties.fill = z(f.properties["2007"])),
+            d'
+
+ndjson-map -r d3 -r d3=d3-scale-chromatic \
+  "$EXP_ESCALA" \
+< geo4-municipios-e-aprendizado-simplificado.json \
+| ndjson-split 'd.features' \
+| geo2svg -n --stroke none -w 1000 -h 600 \
+  > aprendizagem-na-pb-choropleth.svg
--- examinavariaveis.R
+++ examinavariaveis.R
@@ -1,6 +1,9 @@
 require(dplyr)
 
-dados <- read.csv("DomicilioRenda_PB.csv", sep = ";")
+dados <- read.csv("Porcentagemdecrianasde4e5anosnaescola.csv", sep = ";")
+
+
+
 dados$V013 <- as.integer(dados$V013)
 dados$V005 <- as.integer(dados$V005)
 
